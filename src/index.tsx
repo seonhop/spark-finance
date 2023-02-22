161eefe34c9640e476561732714389a765487e11
seonhop <78877741+seonhop@users.noreply.github.com>
2023-02-21 22:13:44 -0500

    theme: setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./Root";
+import { lightTheme } from "./theme";
+import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<Root />
+		<ThemeProvider theme={lightTheme}>
+			<Root />
+		</ThemeProvider>
 	</React.StrictMode>
 );
